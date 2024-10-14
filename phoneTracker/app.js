893884cd6b21736d3c9795f33163fb96fb624252
Pannic <pangl2000@gmail.com>
2024-10-14 14:52:35 +0300

    Ngrok

--- phoneTracker/app.js
+++ phoneTracker/app.js
@@ -4,7 +4,7 @@ const originLongitude = 21.789350;
 
 // Function to get elevation from Google Maps API
 async function getElevation(lat, lng) {
-    const url = `http://localhost:3000/api/elevation?lat=${lat}&lng=${lng}`;
+    const url = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat},${lng}&key=${apiKey}`;
     try {
         const response = await fetch(url);
         const data = await response.json();
